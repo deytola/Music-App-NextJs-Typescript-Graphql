1b5cad10b71f5f0fde0cbd6bd1e1f1fbe23bf67c
adetola <adebolaadetola65@gmail.com>
2023-10-30 20:06:33 -0600

    chore: clean up

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function MusicPage() {
         />
       </div>
       <div className="hidden md:block">
-        <Menu />
+        {/* <Menu /> */}
         <div className="border-t">
           <div className="bg-background">
             <div className="grid lg:grid-cols-5">
